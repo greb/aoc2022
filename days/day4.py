828f0ce20b71ecc4d623b04dafea9c78e4c47b2f
greb <greb>
2022-12-04 11:39:56 +0100

    Solve day 4

---
+++ days/day4.py
@@ -0,0 +1,23 @@
+import re
+
+def parse(inp):
+    pattern = re.compile(r'(\d+)-(\d+),(\d+)-(\d+)')
+    sections = []
+    for line in inp.splitlines():
+        section = pattern.match(line).groups()
+        section = tuple(map(int, section))
+        sections.append(section)
+    return sections
+
+def part1(sections):
+    cnt = 0
+    for a,b,c,d in sections:
+        cnt += a >= c and b <= d or c >= a and d <= b
+    return cnt
+
+
+def part2(sections):
+    cnt = 0
+    for a,b,c,d in sections:
+        cnt += a <= c <= b or a <= d <= b or c <= a <= d or c <= b <= d
+    return cnt
